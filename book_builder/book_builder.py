a659a3b2b73e529e935fd48610f8b84a16e2c319
Steve Russell <stevenrussell@outlook.com>
2020-03-16 22:41:43 -0400

    began collecting verses to build chapter

--- book_builder/book_builder.py
+++ book_builder/book_builder.py
@@ -1,6 +1,8 @@
 from enum import Enum, auto
 from .models.book import Book
 from .models.chapter import Chapter
+from .models.verse import Verse
+import re
 
 
 class LineType(Enum):
@@ -14,6 +16,7 @@ class LineType(Enum):
 class BookBuilderState(Enum):
     ENUMERATING_CHAPTER = auto()
     TITLING_CHAPTER = auto()
+    COLLECTING_VERSES = auto()
 
 
 class BookBuilder:
@@ -26,16 +29,27 @@ class BookBuilder:
 
     def classify(self, text):
         classifiers = {
-            BookBuilderState.ENUMERATING_CHAPTER: _classify_enumerating_chapter
+            BookBuilderState.ENUMERATING_CHAPTER: _classify_enumerating_chapter,
+            BookBuilderState.TITLING_CHAPTER: _classify_titling_chapter,
+            BookBuilderState.COLLECTING_VERSES: _classify_collecting_verses
         }
         classification = classifiers[self.state](text)
         return classification
 
     def process(self, text):
         line_type = self.classify(text)
+
         if line_type == LineType.CHAPTER_NUMBER:
             self.current_chapter = Chapter(int(text))
             self.state = BookBuilderState.TITLING_CHAPTER
+
+        elif line_type == LineType.CHAPTER_TITLE:
+            self.current_chapter.title = text
+
+        elif line_type == LineType.VERSE_CHUNK:
+            verses = parse_verse_chunk(text)
+            self.current_chapter.add_all_verses(verses)
+            self.state = BookBuilderState.COLLECTING_VERSES
 
 
 def _classify_enumerating_chapter(next_line):
@@ -43,3 +57,17 @@ def _classify_enumerating_chapter(next_line):
         return LineType.CHAPTER_NUMBER
     else:
         raise Exception(f"Chapter number expected. Got {next_line} instead.")
+
+
+def _classify_titling_chapter(next_line):
+    return LineType.VERSE_CHUNK if re.match('^\\d+', next_line) else LineType.CHAPTER_TITLE
+
+
+def _classify_collecting_verses(next_line):
+    if re.match('^\\d+', next_line):
+        return LineType.VERSE_CHUNK
+
+
+def parse_verse_chunk(chunk):
+    verse_tuples = re.findall('(\\d+)(\\D+)', chunk)
+    return [Verse(int(n), t.strip()) for n, t in verse_tuples]
--- book_builder/book_builder_test.py
+++ book_builder/book_builder_test.py
@@ -17,7 +17,47 @@ def test_book_builder_first_chapter_title():
     assert len(bb.book.chapters) == 0
 
 
+def test_add_chapter_title():
+    next_line = "Some chapter title"
+    bb = get_builder_for_state(BookBuilderState.TITLING_CHAPTER)
+    assert bb.classify(next_line) == LineType.CHAPTER_TITLE
+    bb.process(next_line)
+    assert bb.state == BookBuilderState.TITLING_CHAPTER
+    assert bb.current_chapter.title == next_line
+
+
+def test_add_first_verse_without_chapter_title():
+    next_line = "1Some verse. 2The next verse."
+    bb = get_builder_for_state(BookBuilderState.TITLING_CHAPTER)
+    assert bb.classify(next_line) == LineType.VERSE_CHUNK
+    bb.process(next_line)
+    assert bb.state == BookBuilderState.COLLECTING_VERSES
+    assert len(bb.current_chapter.verses) == 2
+    assert bb.current_chapter.get_verse(2).text == 'The next verse.'
+
+
+def test_collecting_verses():
+    bb = get_builder_for_state(BookBuilderState.COLLECTING_VERSES)
+    verse_chunk_1 = '3This is a verse. 4Another verse here 5This one ends "with a quote."'
+    verse_chunk_2 = '6Another verse over here. 7Can you read this?'
+    bb.process(verse_chunk_1)
+    bb.process(verse_chunk_2)
+    assert len(bb.current_chapter.verses) == 7
+    assert bb.current_chapter.get_verse(6).text == 'Another verse over here.'
+
+
 def test_classify_number_test():
     bb = BookBuilder("Matthew")
     bb.state = BookBuilderState.ENUMERATING_CHAPTER
     assert bb.classify("13") == LineType.CHAPTER_NUMBER
+
+
+def get_builder_for_state(bb_state):
+    bb = BookBuilder("Matthew")
+    bb.process("1")
+    if bb_state == BookBuilderState.TITLING_CHAPTER:
+        return bb
+    bb.process('Chapter title')
+    bb.process('1First verse. 2Second verse.')
+    if bb_state == BookBuilderState.COLLECTING_VERSES:
+        return bb
