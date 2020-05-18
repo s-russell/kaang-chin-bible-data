68076cd00f878dc0f8b71cc9ee8c078dc02bf70b
Steven Russell <StevenRussell@outlook.com>
2020-05-17 21:07:02 -0400

    added more detailed validation errors

--- models/chapter.py
+++ models/chapter.py
@@ -7,11 +7,11 @@ class Chapter:
 
     def add_verse(self, verse):
         self.verses.append(verse)
-        self.verses.sort(key=lambda v: v.number)
+        # self.verses.sort(key=lambda v: v.number)
 
     def add_all_verses(self, verses):
         self.verses.extend(verses)
-        self.verses.sort(key=lambda v: v.number)
+        # self.verses.sort(key=lambda v: v.number)
 
     def get_verse(self, number):
         verses = [v for v in self.verses if v.number == number]
